4693135d51b2200f47526f914d021788c6fef1f4
milad <me@miladmoafi.com>
2016-06-12 23:42:57 +0430

    Complete Annotations

--- src/main/java/com/miladmoafi/persmatview/PFTextView.java
+++ src/main/java/com/miladmoafi/persmatview/PFTextView.java
@@ -6,21 +6,37 @@ import android.widget.TextView;
 
 public class PFTextView extends TextView {
 
+    /**
+     * Constructor for TextView Attribute Sets
+     * @param context Context
+     * @param attrs Attributes
+     */
     public PFTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initialize();
     }
 
+    /**
+     * Constructor 1
+     * @param context Context
+     */
     public PFTextView(Context context) {
         super(context);
         initialize();
     }
 
+    /**
+     * Get TextView Value and Convert Digits
+     */
     private void initialize(){
         this.setText(ConvertPersian(this.getText().toString()));
     }
 
-    //Milad Moafi
+    /**
+     * Replace English Digits to Persian/Arabic Digits
+     * @param string Feed
+     * @return String
+     */
     private String ConvertPersian(String string) {
         return string.replaceAll("0", "۰")
                 .replaceAll("1", "۱")
