0781a4b1b0e2ad55ec67df0043a036e503c63281
milad <me@miladmoafi.com>
2016-06-12 23:45:07 +0430

    Complete Comment

--- src/main/java/com/miladmoafi/persmatview/MainActivity.java
+++ src/main/java/com/miladmoafi/persmatview/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Draw Custom TextView in Context
         PFTextView pfTextView = new PFTextView(this);
         setContentView(pfTextView);
     }
